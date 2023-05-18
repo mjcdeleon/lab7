17f1565891a1d926af948ab665baa0f550681b96
Macylene J De-Leon <cs15lsp23dw@ieng6-202.ucsd.edu>
2023-05-18 09:18:13 -0700

    Trying for first

--- ListExamplesTests.java
+++ ListExamplesTests.java
@@ -5,14 +5,16 @@ import java.util.ArrayList;
 
 
 public class ListExamplesTests {
-	@Test(timeout = 500)
+	@Test
+	(timeout = 500)
 	public void testMerge1() {
     		List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
 		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
-		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2).toArray());
+		assertArrayEquals(new String[]{ "x", "y", "a", "b"}, ListExamples.merge(l1, l2).toArray());
 	}
 	
-	@Test(timeout = 500)
+	@Test
+	(timeout = 500)
         public void testMerge2() {
 		List<String> l1 = new ArrayList<String>(Arrays.asList("a", "b", "c"));
 		List<String> l2 = new ArrayList<String>(Arrays.asList("c", "d", "e"));
